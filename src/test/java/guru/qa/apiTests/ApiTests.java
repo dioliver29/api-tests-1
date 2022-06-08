8a7bcfbea322f9eb32121faf237ed9b44f40bce5
Diana Stikheeva <d.stikheeva@gmail.com>
2022-06-09 00:44:33 +0500

    added reqres.in api tests

---
+++ src/test/java/guru/qa/apiTests/ApiTests.java
@@ -0,0 +1,76 @@
+package guru.qa.apiTests;
+
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
+
+public class ApiTests {
+
+    @Test
+    void checkTotalPages() {
+        given()
+                .when()
+                .get("https://reqres.in/api/users")
+                .then()
+                .log().body()
+                .body("total_pages", is(2));
+    }
+
+    @Test
+    void createUser() {
+
+        String body = "{ \"name\": \"morpheus\", \"job\": \"leader\" }";
+        String name = "morpheus";
+
+        given()
+                .body(body)
+                .contentType(ContentType.JSON)
+                .when()
+                .post("https://reqres.in/api/users")
+                .then()
+                .statusCode(201)
+                .log().status()
+                .log().body()
+                .body("name", is(name));
+    }
+
+    @Test
+    void notFoundTest() {
+        given()
+                .when()
+                .get("https://reqres.in/api/unknown/23")
+                .then()
+                .log().status()
+                .statusCode(404)
+                .log().body();
+    }
+
+    @Test
+    void updateUserJob() {
+        String body = "{ \"name\": \"morpheus\", \"job\": \"QaGuru\" }";
+        String job = "QaGuru";
+
+        given()
+                .body(body)
+                .contentType(ContentType.JSON)
+                .when()
+                .patch("https://reqres.in/api/user/2")
+                .then()
+                .statusCode(200)
+                .log().status()
+                .log().body()
+                .body("job", is(job));
+    }
+
+    @Test
+    void foundPantoneValueWithId1() {
+        given()
+                .when()
+                .get("https://reqres.in/api/unknown")
+                .then()
+                .log().body()
+                .body("data.findAll { it.id = 1}.pantone_value", hasItem("15-4020"));
+    }
+}
